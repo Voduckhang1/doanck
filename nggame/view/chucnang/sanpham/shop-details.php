ba37948971fcdccba77c081a4b386a35da01ae65
Long <phamtienlong135@gmail.com>
2021-12-20 17:15:58 +0700

    THEM CHUC NANG LOC SAN PHAM THEO DANH MUC VA XEM CHI TIET SAN PHAM

--- nggame/view/chucnang/sanpham/shop-details.php
+++ nggame/view/chucnang/sanpham/shop-details.php
@@ -1,15 +1,18 @@
+<?php
 
+include_once "../controller/shopgriddetailcontroller.php";
+?>
     <!-- Breadcrumb Section Begin -->
     <section class="breadcrumb-section set-bg" data-setbg="img/banner/banner5.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="breadcrumb__text">
-                        <h2>Elden Ring</h2>
+                        <h2><?php echo $row['ten_sp']; ?></h2>
                         <div class="breadcrumb__option">
                             <a href="./index.php">Trang chủ</a>
-                            <a href="./index.php">Shop</a>
-                            <span>Elden Ring</span>
+                            <a href="index.php?page_layout=shop-grid">Shop</a>
+                            <span><?php echo $row['ten_sp']; ?></span>
                         </div>
                     </div>
                 </div>
@@ -26,23 +29,23 @@
                     <div class="product__details__pic">
                         <div class="product__details__pic__item">
                             <img class="product__details__pic__item--large"
-                                src="img/product/elden-ring-ps5.jpg" alt="">
+                                src="../../nggame_admin/view/pictures/<?php echo $row['anh_sp']?>" alt="">
                         </div>
-                        <div class="product__details__pic__slider owl-carousel">
-                            <img data-imgbigurl="img/product/elden-ring-ps5.jpg"
-                                src="img/product/elden-ring-ps5.jpg" alt="">
+                        <!-- <div class="product__details__pic__slider owl-carousel">
+                            <img data-imgbigurl="../../nggame_admin/view/pictures/<?php echo $row['anh_sp']?>"
+                                src="../../nggame_admin/view/pictures/<?php echo $row['anh_sp']?>" alt="">
                             <img data-imgbigurl="img/product/details/elden-ring-41.jpg"
                                 src="img/product/details/elden-ring-41.jpg" alt="">
                             <img data-imgbigurl="img/product/details/elden-ring-42.jpg"
                                 src="img/product/details/elden-ring-42.jpg" alt="">
                             <img data-imgbigurl="img/product/details/elden-ring-43.jpg"
                                 src="img/product/details/elden-ring-43.jpg" alt="">
-                        </div>
+                        </div> -->
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6">
                     <div class="product__details__text">
-                        <h3>Elden Ring</h3>
+                        <h3><?php echo $row['ten_sp']; ?></h3>
                         <div class="product__details__rating">
                             <i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
@@ -51,21 +54,34 @@
                             <i class="fa fa-star-half-o"></i>
                             <span>(18 reviews)</span>
                         </div>
-                        <div ><del>1,580,000₫</del></div>
-                        <div class="product__details__price">1,280,000₫</div>
+                        <div ><del><?php  echo number_format($row['don_gia'], 0, '', '.')." VNĐ"; ?></del></div>
+                        <div class="product__details__price"><?php  echo number_format($row['don_gia'], 0, '', '.')." VNĐ"; ?></div>
                         <div class="product__details__quantity">
-                            <div class="quantity">
+                            <!-- <div class="quantity">
                                 <div class="pro-qty">
                                     <input type="text" value="1">
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
-                        <a href="#" class="primary-btn">Thêm vào giỏ hàng</a>
+                        <a href="chucnang/giohang/themhang.php?id_sp=<?php echo $row['id_sp']?>" class="primary-btn">Thêm vào giỏ hàng</a>
                         <ul>
-                            <li><b>Availability</b> <span>Còn hàng</span></li>
-                            <li><b>Thể loại</b> <span>Adventure, RPG </span></li>
-                            <li><b>Hệ máy</b> <span>PS5, PS4 </span></li>
-                            <li><b>Nhà xuất bản</b> <span>Bandai Namco Games </span></li>
+                            <li><b>Availability</b> <span><?php  echo $row['tinh_trang']; ?></span></li>
+                            <li><b>Thể loại</b> <span>
+                                <?php   
+                                //So sánh id_dm của sản phẩm và id_danhmuc của danhmucsp
+                                while ($rowDm = mysqli_fetch_array($querydm)){
+                                if($row['id_danhmuc']==$rowDm['id_dm_sp']){
+                                    echo $rowDm['ten_dm_sp'];
+                                }
+                            }?></span></li>
+                            <li><b>Hệ máy</b> <span><?php echo $row['he_may'];?> </span></li>
+                            <li><b>Nhà xuất bản</b> <span>
+                                <?php   
+                                while ($rowcc = mysqli_fetch_array($querycc)){
+                                if($row['id_ncc']==$rowcc['id_ncc']){
+                                    echo $rowcc['ten_ncc'];
+                                }
+                            }?> </span></li>
                             <li><b>Share on</b>
                                 <div class="share">
                                     <a href="#"><i class="fa fa-facebook"></i></a>
@@ -94,23 +110,8 @@
                            
                             <div class="tab-pane" id="tabs-2" role="tabpanel">
                                 <div class="product__details__tab__desc">
-                                    <h6>Products Infomation</h6>
-                                    <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
-                                        Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus.
-                                        Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam
-                                        sit amet quam vehicula elementum sed sit amet dui. Donec rutrum congue leo
-                                        eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat.
-                                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent
-                                        sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac
-                                        diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante
-                                        ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                                        Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
-                                        Proin eget tortor risus.</p>
-                                    <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem
-                                        ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet
-                                        elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum
-                                        porta. Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus
-                                        nibh. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
+                                <h6>Nội dung của game</h6>
+                                    <p><?php echo $row['noi_dung']; ?></p>
                                 </div>
                             </div>
                             <div class="tab-pane" id="tabs-3" role="tabpanel">
@@ -148,11 +149,12 @@
                 </div>
             </div>
             <div class="row">
-            <div class="col-lg-3 col-md-4 col-sm-6 mix Action FPS">
+                <?php include_once 'sanphamtuongtu.php'; ?>
+            <!-- <div class="col-lg-3 col-md-4 col-sm-6 mix Action FPS">
                     <div class="featured__item">
                         <div class="featured__item__pic set-bg" data-setbg="img/product/call-of-duty-vanguard-ps5.jpg">
                             <ul class="featured__item__pic__hover">
-                                <!-- <li><a href="shop-grid.php"><i class="fa fa-heart"></i></a></li> -->
+                                <li><a href="shop-grid.php"><i class="fa fa-heart"></i></a></li>
                                 <li><a href="shop-grid.php"><i class="fa fa-retweet"></i></a></li>
                                 <li><a href="shoping-cart.php"><i class="fa fa-shopping-cart"></i></a></li>
                             </ul>
@@ -167,7 +169,7 @@
                     <div class="featured__item">
                         <div class="featured__item__pic set-bg" data-setbg="img/product/marvels-guardians-of-the-galaxy-ps5.jpg">
                             <ul class="featured__item__pic__hover">
-                                <!-- <li><a href="shop-grid.php"><i class="fa fa-heart"></i></a></li> -->
+                                <li><a href="shop-grid.php"><i class="fa fa-heart"></i></a></li>
                                 <li><a href="shop-grid.php"><i class="fa fa-retweet"></i></a></li>
                                 <li><a href="shoping-cart.php"><i class="fa fa-shopping-cart"></i></a></li>
                             </ul>
@@ -182,7 +184,7 @@
                     <div class="featured__item">
                         <div class="featured__item__pic set-bg" data-setbg="img/product/battlefield-2042-ps5.jpg">
                             <ul class="featured__item__pic__hover">
-                                <!-- <li><a href="shop-grid.php"><i class="fa fa-heart"></i></a></li> -->
+                                <li><a href="shop-grid.php"><i class="fa fa-heart"></i></a></li>
                                 <li><a href="shop-grid.php"><i class="fa fa-retweet"></i></a></li>
                                 <li><a href="shoping-cart.php"><i class="fa fa-shopping-cart"></i></a></li>
                             </ul>
@@ -198,7 +200,7 @@
                         <div class="product__discount__item__pic set-bg" data-setbg="img/product/tales-of-arise-ps5.jpg">
                         <div class="product__discount__percent">-20%</div>
                                 <ul class="product__item__pic__hover">
-                                <!-- <li><a href="shop-grid.php"><i class="fa fa-heart"></i></a></li> -->
+                                <li><a href="shop-grid.php"><i class="fa fa-heart"></i></a></li>
                                 <li><a href="shop-grid.php"><i class="fa fa-retweet"></i></a></li>
                                 <li><a href="shoping-cart.php"><i class="fa fa-shopping-cart"></i></a></li>
                             </ul>
@@ -208,7 +210,7 @@
                                 <div class="product__item__price">1,280,000₫<span>1,580,000₫</span></div>
                             </div>
                     </div>
-                </div>
+                </div> -->
         </div>
     </section>
     <!-- Related Product Section End -->
